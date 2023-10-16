22ccbe5ac84b71331be638a248b94e92bd501064
jose <jose.s.contacto@gmail.com>
2023-10-16 15:57:11 -0300

    Create class Note Taker into old script
    Implemente la funcionalidad anterior en el script create_note antiguo,
    creandole una clase NoteTaker para modularizarlo.

--- tests/test_take_note.py
+++ tests/test_take_note.py
@@ -1,14 +1,21 @@
-class NoteTaker:
+import pytest
+import os
+from src.note_taker.create_note import NoteTaker
+
+class FakeNoteTaker:
+    def __init__(self,note_path:str,note_templ:str):
+        self._notes_path = note_path
+        self._notes_templ = note_templ
+
     def notes_path(self):
-        return "/home/peace/notes/"
+        return self._notes_path
 
     def notes_templ(self):
-        return "/home/peace/notes/template.md"
+        return self._notes_templ
 
-        return
 def test_note_taker_should_exist_by_the_note_folder_paths_and_a_template_path():
     notes_path = "/home/peace/notes/"
     notes_templ = "/home/peace/notes/template.md"
-    nt = NoteTaker()
+    nt = NoteTaker(notes_path, notes_templ)
     assert nt.notes_path() == notes_path
     assert nt.notes_templ() == notes_templ
