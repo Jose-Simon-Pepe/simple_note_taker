e138608aa4c1a4f41414c283b6e1de3cc8e3bf63
jose <jose.s.contacto@gmail.com>
2023-10-18 12:02:22 -0300

    #2: Define repository interface

---
+++ src/infra/repo_notes.py
@@ -0,0 +1,22 @@
+"""
+Repo interface for CrUD notes
+Entity: Note
+Methods: 
+    - get(id:int)-> A note  
+    - get(name:str)-> A list of string pattern matching notes
+    - save(note_dto)-> Boolean depending on op status
+    - update(id:int)-> Boolean depending on op status
+    - get_all()-> A list of all existing notes
+"""
+
+class RepoNotes:
+    def get(id:int)-> Note:  
+        pass
+    def get(name:str)-> list:
+        pass
+    def get_all()-> list:
+        pass
+    def save(note_dto)-> bool:
+        pass
+    def update(id:int)-> bool:
+        pass
